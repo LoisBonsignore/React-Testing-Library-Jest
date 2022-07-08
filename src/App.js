414a965261bf49b53c3bd997aaf65e5ca8c061ce
Lois Bonsignore <bonsignore.lois@gmail.com>
2022-07-08 18:06:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
+  const a = 2;
+  const b = 4;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,7 +21,15 @@ function App() {
         >
           Learn React
         </a>
+        <ul>
+          <li>Apple</li>
+          <li>Banana</li>
+          <li>Orange</li>
+        </ul>
+        <h1 data-testid="mytestid">Hello</h1>
+        <span title="sum">{a + b}</span>
       </header>
+      <Login />
     </div>
   );
 }
